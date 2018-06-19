96ab091bddeb7a12a978bfbd531d22a892997ee3
USER <USER@com201-04>
2018-06-19 15:13:53 +0900

    recommit

---
+++ ssample/src/main/java/com/myin/sample/controller/nodab.java
@@ -0,0 +1,11 @@
+package com.myin.sample.controller;
+
+public class nodab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("깃허브 생각보다 복잡하다");
+	}
+
+}
